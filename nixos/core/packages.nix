1536a879960406c6e1627736b6d76f1c4d50dd6c
phobia8542 <phobia8542@gmail.com>
2025-12-02 20:31:08 -0500

    moved protonVPN & speedtest from pentest to core packages

--- nixos/core/packages.nix
+++ nixos/core/packages.nix
@@ -23,12 +23,14 @@
     mediainfo # Supplies technical and tag information about a video or audio file
     mpv # General-purpose media player
     networkmanager-openvpn # NetworkManager's OpenVPN plugin
-    nvtopPackages.full # (h)top like task monitor for AMD, Adreno, Intel and NVIDIA GPUs
+    # nvtopPackages.full # (h)top like task monitor for AMD, Adreno, Intel and NVIDIA GPUs
     obsidian # Powerful knowledge base that works on top of a local folder of plain text Markdown files
     openvpn # Robust and highly flexible tunneling application
     pavucontrol # PulseAudio Volume Control
     playerctl # Command-line utility and library for controlling media players that implement
+    protonvpn-gui # Proton VPN GTK app for linux
     ranger # File manager with minimalistic curses interface
+    speedtest-rs # Command line internet speedtest tool written in rust
     thunderbird # full-featured e-mail client
     unzip # Extraction utility for archives compressed in .zip format
     vencord # Cutest Discord client mod (Allows for customization of discord clients)
