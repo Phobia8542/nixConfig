51a6de43f67a48d668c76bb03ff2158c4f622a4e
phobia8542 <phobia8542@gmail.com>
2025-12-04 10:20:10 -0500

    updated hardware (more drives)

--- hosts/artemis/hardware-configuration.nix
+++ hosts/artemis/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
@@ -24,17 +24,19 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
+  fileSystems."/mnt/bulk2" =
+    { device = "/dev/disk/by-uuid/1bbc7515-b014-48b1-8228-8dcab3e8689d";
+      fsType = "ext4";
+    };
+
+  fileSystems."/mnt/gameDriveSmall" =
+    { device = "/dev/disk/by-uuid/55523ad9-97b3-49b9-8f5d-0ab422056cbb";
+      fsType = "ext4";
+    };
+
   swapDevices =
     [ { device = "/dev/disk/by-uuid/219aa5a3-5da1-44c6-9b84-d81247c50157"; }
     ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp7s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp6s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
