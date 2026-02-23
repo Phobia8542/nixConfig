78411aab1bbdc727dbcc9dc7e9003576643e0f0d
phobia8542 <phobia8542@gmail.com>
2026-02-23 16:58:54 -0500

    added libva-utils to content

--- hosts/xerxes/local.nix
+++ hosts/xerxes/local.nix
@@ -2,6 +2,8 @@
   environment.systemPackages = with pkgs; [
 
   btop-rocm # Monitor of resources
+  # xf86-video-amdgpu # Xorg driver for AMD Radeon GPUs using the amdgpu kernel driver
+
 
   ];
 }
--- nixos/content/packages.nix
+++ nixos/content/packages.nix
@@ -4,6 +4,7 @@
 
     audacity # Sound editor with graphical UI
     libva # Implementation for VA-API (Video Acceleration API)
+    libva-utils # Collection of utilities and examples for VA-API
     libvdpau # Library to use Video Decode and Presentation API for Unix (VDPAU)
     kdePackages.kdenlive # Free and open source video editor, based on MLT Framework and KDE Frameworksq
     shaderc # Collection of tools, libraries and tests for shader compilation
